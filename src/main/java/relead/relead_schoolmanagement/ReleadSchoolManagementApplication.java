d33586c65e3a880535c34c57395a21cf5eb43f4f
SeifeddineLamti <seifeddine.lamti@esprit.tn>
2025-12-02 14:53:46 +0100

    Creation

---
+++ src/main/java/relead/relead_schoolmanagement/ReleadSchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package relead.relead_schoolmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleadSchoolManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleadSchoolManagementApplication.class, args);
+    }
+
+}
